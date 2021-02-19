8a22c39fe1041287ec822e02ac7355f0012c1c8c
Ahmad Osman <ahmad@ahmad-osman.com>
2021-02-19 11:46:13 +0100

    ejs start

--- 1-blog/app.js
+++ 1-blog/app.js
@@ -8,9 +8,20 @@ const port = process.env.PORT || 3000;
 // make any file inside public folder accessable without creating routes for each file
 app.use(express.static(__dirname + '/public'));
 
+// set ejs as a view engine
+app.set('view engine', 'ejs');
+// set the views folder
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
     // console.log(__dirname);
-    res.sendFile( __dirname +  '/views/index.html');
+    //res.sendFile( __dirname +  '/views/index.html');
+    res.render('index');
+})
+app.get('/about', (req, res) => {
+    // console.log(__dirname);
+    // res.sendFile( __dirname +  '/views/about.html');
+    res.render('about');
 })
 
 app.listen(port, () => {
