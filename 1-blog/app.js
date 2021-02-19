a81e0d4d9df87286eb457a8b0584eb557649b6ee
Ahmad Osman <ahmad@ahmad-osman.com>
2021-02-19 15:42:33 +0100

    start render data

--- 1-blog/app.js
+++ 1-blog/app.js
@@ -24,6 +24,46 @@ app.get('/about', (req, res) => {
     res.render('about');
 })
 
+
+// create a comments array to save the comments inside
+
+const commentsArr = [
+    {
+        name: "Ahmad",
+        commentDate: new Date("18.02.2021"),
+        comment: "Hello this is my first comment"
+    },
+    {
+        name: "Basem",
+        commentDate: new Date("17.02.2021"),
+        comment: "Hello I am Basem"
+    },
+    {
+        name: "Nisreen",
+        commentDate: new Date("16.02.2021"),
+        comment: "I liked this blog"
+    }
+];
+
+
+app.get('/blog', (req, res) => {
+    // console.log(__dirname);
+    // res.sendFile( __dirname +  '/views/about.html');
+    res.render('blog');
+})
+
+app.get('/contact', (req, res) => {
+    // console.log(__dirname);
+    // res.sendFile( __dirname +  '/views/about.html');
+    res.render('contact');
+})
+
+app.get('/marketing', (req, res) => {
+    // console.log(__dirname);
+    // res.sendFile( __dirname +  '/views/about.html');
+    res.render('marketing');
+})
+
 app.listen(port, () => {
     console.log(`App is running on port: ${port}` );
 })
