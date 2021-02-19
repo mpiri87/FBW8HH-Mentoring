66f9853977ca48ec6cb1d92f2cc7737dd5bb7881
Memoleee <piri.mehmet@googlemail.com>
2021-02-19 10:14:43 +0100

    Second-Commit

---
+++ mentoring-19022021/1-Blog/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.port || 3000; // first get the port from the computer or 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`); // this way with backticks is called string incection
+});
